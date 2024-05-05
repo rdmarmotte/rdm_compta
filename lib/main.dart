aa91c9be4a8beaa7962c10369e1cb83ca7d53ff3
Brice Meunier <github@rdmarmotte.net>
2024-05-05 17:45:35 +0200

    Second visual test

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("rdmCompta"),
-        ),
-        body: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                    "Bienvenue sur l'appli compta",
+      home: HomePage()
+    );
+  }
+}
+
+class AccountDisplay extends StatelessWidget {
+  const AccountDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("rdmCompta"),
+      ),
+      body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                  "Bienvenue sur l'appli compta",
+                style: TextStyle(
+                  fontSize: 22
+                )
+              ),
+              Text(
+                  "Compte tes sous Picsou !",
                   style: TextStyle(
-                    fontSize: 22
+                      fontSize: 18
                   )
-                ),
-                Text(
-                    "Compte tes sous Picsou !",
-                    style: TextStyle(
-                        fontSize: 18
-                    )
-                ),
-              ],
-            )
-        ),
-      )
+              ),
+            ],
+          )
+      ),
     );
   }
 }
